be28bf942921d51a71c55b6afd9886d3edd4f11d
mouse2233 <scilikh2@yandex.ru>
2021-10-10 22:41:52 +0300

    базовое обращение к бд + dataGrid

--- myWidget/Form1.Designer.cs
+++ myWidget/Form1.Designer.cs
@@ -33,10 +33,15 @@
             this.buttonTest = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.dateLabel = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.checkBoxPoverh = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -47,6 +52,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -55,9 +61,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(153, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Инит";
+            this.button1.Text = "получить список заданий";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -65,9 +71,9 @@
             // 
             this.buttonTest.Location = new System.Drawing.Point(153, 29);
             this.buttonTest.Name = "buttonTest";
-            this.buttonTest.Size = new System.Drawing.Size(139, 23);
+            this.buttonTest.Size = new System.Drawing.Size(150, 23);
             this.buttonTest.TabIndex = 2;
-            this.buttonTest.Text = "Однократно нарисовать";
+            this.buttonTest.Text = "Отправить задание";
             this.buttonTest.UseVisualStyleBackColor = true;
             this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
             // 
@@ -75,9 +81,9 @@
             // 
             this.button2.Location = new System.Drawing.Point(154, 58);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.Size = new System.Drawing.Size(149, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Старт часов";
+            this.button2.Text = "\\t";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -85,23 +91,16 @@
             // 
             this.button3.Location = new System.Drawing.Point(153, 87);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(150, 23);
             this.button3.TabIndex = 4;
             this.button3.Text = "Stop";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(289, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(142, 147);
-            this.listBox1.TabIndex = 5;
-            // 
             // dateLabel
             // 
             this.dateLabel.AutoSize = true;
+            this.dateLabel.BackColor = System.Drawing.SystemColors.Window;
             this.dateLabel.Location = new System.Drawing.Point(54, 150);
             this.dateLabel.Name = "dateLabel";
             this.dateLabel.Size = new System.Drawing.Size(0, 13);
@@ -111,20 +110,68 @@
             // 
             this.button4.Location = new System.Drawing.Point(154, 117);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(149, 23);
             this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
+            this.button4.Text = "exit";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBoxPoverh
+            // 
+            this.checkBoxPoverh.AutoSize = true;
+            this.checkBoxPoverh.Checked = true;
+            this.checkBoxPoverh.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxPoverh.Location = new System.Drawing.Point(154, 145);
+            this.checkBoxPoverh.Name = "checkBoxPoverh";
+            this.checkBoxPoverh.Size = new System.Drawing.Size(116, 17);
+            this.checkBoxPoverh.TabIndex = 8;
+            this.checkBoxPoverh.Text = "Поверх всех окон";
+            this.checkBoxPoverh.UseVisualStyleBackColor = true;
+            this.checkBoxPoverh.CheckedChanged += new System.EventHandler(this.checkBoxPoverh_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(905, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(905, 27);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(161, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(905, 54);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(161, 20);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(310, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(589, 150);
+            this.dataGridView1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 179);
+            this.ClientSize = new System.Drawing.Size(1078, 179);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBoxPoverh);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.dateLabel);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.buttonTest);
@@ -134,6 +181,7 @@
             this.Text = "Main";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,9 +194,13 @@
         private System.Windows.Forms.Button buttonTest;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label dateLabel;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBoxPoverh;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- myWidget/Form1.cs
+++ myWidget/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Threading;
 using System.Diagnostics;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace myWidget
 {
@@ -21,11 +22,15 @@ namespace myWidget
         public Form1()
         {
             InitializeComponent();
-            TopMost = true;
             this.TransparencyKey = this.BackColor;
             this.FormBorderStyle = FormBorderStyle.None; //настройка формы без рамки
             dateLabel.Text = DateTime.Now.Day.ToString() + "." + DateTime.Now.Month.ToString() + "." + DateTime.Now.Year.ToString();
             startTime = DateTime.Now;
+            dateLabel.BackColor = Color.White;
+            testClock = new MyClock(pictureBox1); //init часов
+            clockThread = new Thread(drawClock); //инит потока часов
+            clockThread.Start(); //старт потока часов
+
         }
         void uploadData()//ждет пока можно будет отправить данные о времени работы
         {
@@ -42,12 +47,81 @@ namespace myWidget
 
         private void button1_Click(object sender, EventArgs e)
         {
-            testClock = new MyClock(pictureBox1);
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=myWidget;Integrated Security=True";
+            string sqlExpression = "select * from Tasks";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                // создаем параметр для имени
+                //                SqlParameter nameParam = new SqlParameter("@name", name);
+                // добавляем параметр к команде
+                //                command.Parameters.Add(nameParam);
+                // создаем параметр для возраста
+                //                SqlParameter ageParam = new SqlParameter("@age", age);
+                // добавляем параметр к команде
+                //                command.Parameters.Add(ageParam);
+                SqlDataReader number = command.ExecuteReader();
+                //                Console.WriteLine("Добавлено объектов: {0}", number); // 1
+                
+                List<string[]> data = new List<string[]>();
+                while (number.Read())
+                {
+                    data.Add(new string[6]);
+
+                    data[data.Count - 1][0] = number[0].ToString();
+                    data[data.Count - 1][1] = number[1].ToString();
+                    data[data.Count - 1][2] = number[2].ToString();
+                    data[data.Count - 1][3] = number[3].ToString();
+                    data[data.Count - 1][4] = number[4].ToString();
+                    data[data.Count - 1][5] = number[5].ToString();
+                }
+                connection.Close();
+                number.Close();
+                var column1 = new DataGridViewColumn();
+                column1.HeaderText = "поле1";
+                column1.Name = "поле2";
+                column1.CellTemplate = new DataGridViewTextBoxCell();
+
+                var column2 = new DataGridViewColumn();
+                column2.HeaderText = "поле1";
+                column2.Name = "поле2";
+                column2.CellTemplate = new DataGridViewTextBoxCell();
+
+                var column3 = new DataGridViewColumn();
+                column3.HeaderText = "поле1";
+                column3.Name = "поле2";
+                column3.CellTemplate = new DataGridViewTextBoxCell();
+
+                var column4 = new DataGridViewColumn();
+                column4.HeaderText = "поле1";
+                column4.Name = "поле2";
+                column4.CellTemplate = new DataGridViewTextBoxCell();
+
+                var column5 = new DataGridViewColumn();
+                column5.HeaderText = "поле1";
+                column5.Name = "поле2";
+                column5.CellTemplate = new DataGridViewTextBoxCell();
+
+                var column6 = new DataGridViewColumn();
+                column6.HeaderText = "поле1";
+                column6.Name = "поле2";
+                column6.CellTemplate = new DataGridViewTextBoxCell();
+
+                dataGridView1.Columns.Add(column1);
+                dataGridView1.Columns.Add(column2);
+                dataGridView1.Columns.Add(column3);
+                dataGridView1.Columns.Add(column4);
+                dataGridView1.Columns.Add(column5);
+                dataGridView1.Columns.Add(column6);
+                foreach (string[] s in data)
+                    dataGridView1.Rows.Add(s);
+            }
         }
 
         private void buttonTest_Click(object sender, EventArgs e)
         {
-            testClock.draveTime();
+            
         }
 
         private bool moveForm = false;
@@ -71,11 +145,66 @@ namespace myWidget
 
         private void button2_Click(object sender, EventArgs e)
         {
-            clockThread = new Thread(drawClock);
-            clockThread.Start();
+             // создадим таблицу вывода товаров с колонками
+             //Название, Цена, Остаток
+
+            var column1 = new DataGridViewColumn();
+            column1.HeaderText = "Название"; //текст в шапке
+            column1.Width = 100; //ширина колонки
+            column1.ReadOnly = true; //значение в этой колонке нельзя править
+            column1.Name = "name"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+            column1.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
+            column1.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+
+            var column2 = new DataGridViewColumn();
+            column2.HeaderText = "Цена";
+            column2.Name = "price";
+            column2.CellTemplate = new DataGridViewTextBoxCell();
+
+            var column3 = new DataGridViewColumn();
+            column3.HeaderText = "Остаток";
+            column3.Name = "count";
+            column3.CellTemplate = new DataGridViewTextBoxCell();
+
+            dataGridView1.Columns.Add(column1);
+            dataGridView1.Columns.Add(column2);
+            dataGridView1.Columns.Add(column3);
+
+            dataGridView1.AllowUserToAddRows = false; //запрешаем пользователю самому добавлять строки
+
+            for (int i = 0; i < 5; ++i)
+            {
+                //Добавляем строку, указывая значения колонок поочереди слева направо
+                dataGridView1.Rows.Add("Пример 1, Товар " + i, i * 1000, i);
+            }
+
+            for (int i = 0; i < 5; ++i)
+            {
+                //Добавляем строку, указывая значения каждой ячейки по имени (можно использовать индекс 0, 1, 2 вместо имен)
+                dataGridView1.Rows.Add();
+                dataGridView1["name", dataGridView1.Rows.Count - 1].Value = "Пример 2, Товар " + i;
+                dataGridView1["price", dataGridView1.Rows.Count - 1].Value = i * 1000;
+                dataGridView1["count", dataGridView1.Rows.Count - 1].Value = i;
+            }
+
+            //А теперь простой пройдемся циклом по всем ячейкам
+            for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; ++j)
+                {
+                    //Значения ячеек хряняться в типе object
+                    //это позволяет хранить любые данные в таблице
+                    object o = dataGridView1[j, i].Value;
+                }
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
             if (clockThread is null)
             {
@@ -86,20 +215,42 @@ namespace myWidget
                 if (clockThread.IsAlive)
                     clockThread.Abort();
             }
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
             this.Close();
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            if (clockThread is null)
+            {
+
+            }
+            else
+            {
+                if (clockThread.IsAlive)
+                    clockThread.Abort();
+            }
             DateTime stopTime = DateTime.Now; //время запуска
             StreamWriter f = new StreamWriter("D:/tempTime.txt");
             f.WriteLine(startTime);
             f.WriteLine(stopTime);
             f.Close();
+        }
+
+        private void checkBoxPoverh_CheckedChanged(object sender, EventArgs e)
+        {
+            TopMost = checkBoxPoverh.Checked;
+        }
+
+        private void visible(bool type)
+        {
+            if (type)
+            {
+
+            }
+        }
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            visible(true);
         }
     }
 }
